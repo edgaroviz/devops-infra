50f5563aaa0f30a3c566e4d4601081a145bfa9f1
Edgar Green <edgar.green@cybereason.com>
2025-01-31 18:06:44 +0200

    vpc module and start of eks

---
+++ tf-infra/modules/vpc/main.tf
@@ -0,0 +1,38 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name                 = "${var.environment}-ze-vpc"
+  cidr                 = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  
+  public_subnets  = var.public_subnets
+  
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_vpn_gateway = false
+  enable_ipv6        = false
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
