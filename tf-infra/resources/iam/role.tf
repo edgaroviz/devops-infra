ec6436f54625f8fa9ffc095ef04569ca470a5d0a
Edgar Green <edgar.green@cybereason.com>
2025-02-07 00:42:57 +0200

    actions refinement

--- tf-infra/resources/iam/role.tf
+++ tf-infra/resources/iam/role.tf
@@ -1,6 +1,39 @@
-resource "aws_iam_role" "ze_my_ecr_role" {
-  name = "ze-my-ecr-role"
-  depends_on = [ aws_iam_policy.ecr_policy ]
+# resource "aws_iam_role" "ze_my_ecr_role" {
+#   name = "ze-my-ecr-role"
+#   depends_on = [ aws_iam_policy.ze_my_ecr_policy ]
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Federated = "arn:aws:iam::920373021859:oidc-provider/token.actions.githubusercontent.com"
+#         }
+#         Action = "sts:AssumeRoleWithWebIdentity"
+#         Condition = {
+#           StringEquals = {
+#             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+#           }
+#           StringLike = {
+#             "token.actions.githubusercontent.com:sub" = [
+#               "repo:edgaroviz/devops-infra:*"
+#             ]
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "ze_my_ecr_role_attachment" {
+#   depends_on = [ aws_iam_role.ze_my_ecr_role ]
+#   role       = aws_iam_role.ze_my_ecr_role.name
+#   policy_arn = aws_iam_policy.ze_my_ecr_policy.arn
+# }
+
+resource "aws_iam_role" "ze_my_admin_role" {
+  name = "AdminRole"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -16,34 +49,10 @@ resource "aws_iam_role" "ze_my_ecr_role" {
           }
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
-              "repo:edgaroviz/devops-infra:*",
-              "repo:edgaroviz/counter-service:*"
+              "repo:edgaroviz/devops-infra:*"
             ]
           }
         }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ze_my_ecr_role_attachment" {
-  depends_on = [ aws_iam_role.ze_my_ecr_role ]
-  role       = aws_iam_role.ze_my_ecr_role.name
-  policy_arn = aws_iam_policy.ecr_policy.arn
-}
-
-resource "aws_iam_role" "ze_my_admin_role" {
-  name = "AdminRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::YOUR_ACCOUNT_ID:root"
-        },
-        Action = "sts:AssumeRole"
       }
     ]
   })
