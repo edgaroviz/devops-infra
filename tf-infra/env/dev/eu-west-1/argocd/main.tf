3fca535bc645356040261ad7dccf92fe8bf2fce6
Edgar Green <edgar.green@cybereason.com>
2025-02-08 22:56:46 +0200

    add create ns

--- tf-infra/env/dev/eu-west-1/argocd/main.tf
+++ tf-infra/env/dev/eu-west-1/argocd/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "7.4.5"
-
+  create_namespace = true
   name      = "argocd"
   namespace = "argocd"
 
