fdc7ab7016bc69422aec407a28caf865a1c2384d
Edgar Green <edgar.green@cybereason.com>
2025-02-09 03:45:31 +0200

    add force delete to ecr

--- tf-infra/resources/ecr/main.tf
+++ tf-infra/resources/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "ze_my_ecr" {
   name                 = "ze_my_ecr"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
